2ce2fa22a9e0ba06eab0abe18bf3b02515c742b4
Serhii Plis <serzh2577@gmail.com>
2022-10-29 20:55:53 +0300

    updd

--- src/index.js
+++ src/index.js
@@ -446,9 +446,9 @@ function populateTextarea() {
     return massage;
   }
 
-  console.log(likes([])); //  "no one likes this"
-  console.log(likes(['Peter'])); //  "Peter likes this"
-  console.log(likes(['Jacob', 'Alex'])); //  "Jacob and Alex like this"
-  console.log(likes(['Max', 'John', 'Mark'])); //  "Max, John and Mark like this"
-  console.log(likes(['Alex', 'Jacob', 'Mark', 'Max'])); //  "Alex, Jacob and 2 others like this"
+  // console.log(likes([])); //  "no one likes this"
+  // console.log(likes(['Peter'])); //  "Peter likes this"
+  // console.log(likes(['Jacob', 'Alex'])); //  "Jacob and Alex like this"
+  // console.log(likes(['Max', 'John', 'Mark'])); //  "Max, John and Mark like this"
+  // console.log(likes(['Alex', 'Jacob', 'Mark', 'Max'])); //  "Alex, Jacob and 2 others like this"
 }
